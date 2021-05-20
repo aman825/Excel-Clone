06f52e59985bff441e3d4f8cda630dab89c65b67
Ishtiyaque Ahmad <ishtiyaqueahmad@Macbook-Air.local>
2021-05-21 00:08:26 +0530

    Till Row Numbering

---
+++ grid.js
@@ -0,0 +1,16 @@
+let leftCol = document.querySelector(".left_col");
+let topRow = document.querySelector(".top_row")
+let grid = document.querySelector(".grid")
+let rows = 100;
+let cols = 26;
+//left_row
+for(let i = 0; i < rows; i++) {
+   let colBox = document.createElement("div");
+   colBox.innerText = i + 1;
+   colBox.setAttribute("class", "box");
+   leftCol.appendChild(colBox);
+}
+for(let i = 0; i < cols; i++) {
+    let cell = document.createElement("div");
+	cell.innerText = String.fromCharCode(65 + i)
+}
