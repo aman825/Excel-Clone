01ec5fad00fb4dfa523b0db2b3a2ddd1b2bd502e
aman82500 <amanambar@gmail.com>
2021-05-24 16:14:20 +0530

    till rows and column

--- grid.js
+++ grid.js
@@ -1,16 +1,20 @@
 let leftCol = document.querySelector(".left_col");
-let topRow = document.querySelector(".top_row")
-let grid = document.querySelector(".grid")
+let topRow = document.querySelector(".top_row");
+let grid = document.querySelector(".grid");
 let rows = 100;
 let cols = 26;
-//left_row
-for(let i = 0; i < rows; i++) {
-   let colBox = document.createElement("div");
-   colBox.innerText = i + 1;
-   colBox.setAttribute("class", "box");
-   leftCol.appendChild(colBox);
+//left row
+for (let i = 0; i < rows; i++)
+{
+    let colBox = document.createElement("div");
+    colBox.innerText = i + 1;
+    colBox.setAttribute("class", "box");
+    leftCol.appendChild(colBox);
 }
-for(let i = 0; i < cols; i++) {
+for (let i = 0; i < cols; i++)
+{
     let cell = document.createElement("div");
-	cell.innerText = String.fromCharCode(65 + i)
+    cell.innerText = String.fromCharCode(65 + i);
+    cell.setAttribute("class", "cell");
+    topRow.appendChild(cell);
 }
