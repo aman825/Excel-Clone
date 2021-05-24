d9d2e29fcda2a2a872807abe789d28dc5c2d8d9c
aman82500 <amanambar@gmail.com>
2021-05-24 17:50:16 +0530

    styling complete almost

--- grid.js
+++ grid.js
@@ -4,17 +4,29 @@ let grid = document.querySelector(".grid");
 let rows = 100;
 let cols = 26;
 //left row
-for (let i = 0; i < rows; i++)
-{
-    let colBox = document.createElement("div");
-    colBox.innerText = i + 1;
-    colBox.setAttribute("class", "box");
-    leftCol.appendChild(colBox);
+for (let i = 0; i < rows; i++) {
+	let colBox = document.createElement("div");
+	colBox.innerText = i + 1;
+	colBox.setAttribute("class", "box");
+	leftCol.appendChild(colBox);
 }
-for (let i = 0; i < cols; i++)
-{
-    let cell = document.createElement("div");
-    cell.innerText = String.fromCharCode(65 + i);
-    cell.setAttribute("class", "cell");
-    topRow.appendChild(cell);
+for (let i = 0; i < cols; i++) {
+	let cell = document.createElement("div");
+	cell.innerText = String.fromCharCode(65 + i);
+	cell.setAttribute("class", "cell");
+	topRow.appendChild(cell);
+}
+for (let i = 0; i < rows; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 0; j < cols; j++) {
+        let cell = document.createElement("div");
+        // cell.innerText=`${String.fromCharCode(65 + j)}  ${i+1}`
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("rid", i);
+        cell.setAttribute("cid", j);
+        cell.setAttribute("contenteditable", "true");
+        row.appendChild(cell);
+    }
+    grid.appendChild(row);
 }
